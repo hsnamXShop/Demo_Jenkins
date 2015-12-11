8a5a1e6186c550380195c347453a128eefe37546
hsnamXShop <hsnam.xshop@gmail.com>
2015-12-12 04:19:08 +0700

    Change Unit Test

--- src/test/java/KCPM/Demo_Jenkins_Java/TestUnit.java
+++ src/test/java/KCPM/Demo_Jenkins_Java/TestUnit.java
@@ -13,7 +13,7 @@ public class TestUnit extends Function
 	@Test
 	public void TestCongHaiSoNguyen()
 	{
-		Assert.assertEquals(3, CongHaiSoNguyen(1, 2));
+		Assert.assertEquals(2, CongHaiSoNguyen(1, 2));
 	}
 	
 	
